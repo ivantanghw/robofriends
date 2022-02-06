2b90872898f74fe14abba117d9357b290ed72771
ivantanghw <ivantanghw@gmail.com>
2022-02-06 23:55:39 +0800

    structure is built

--- src/Card.js
+++ src/Card.js
@@ -1,15 +1,16 @@
 import React from 'react'
 
-const Card = () => {
+const Card = ({id, name, email}) => {
     return (
-        <div>
-            <img alt='robots' src='' />
+        // reference: http://tachyons.io/docs/table-of-properties/
+        <div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5 tc'>
+            <img alt='robots' src={`https://robohash.org/${id}?200x200`} />
             <div>
-                <h2>Jane Doe</h2>
-                <p>jane.doe@gmail.com</p>
+                <h2>{name}</h2>
+                <p>{email}</p>
             </div>
         </div>
     )
-}
+};
 
-export default Card
+export default Card;
